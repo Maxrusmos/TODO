0fb212534a120c4df7456bd25304be95daf73398
Maxim Vorobyev <vmaxim7777@gmail.com>
2022-05-09 10:17:36 +0300

    fixed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,9 +4,11 @@ import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 const Todo = ({ todos, completeTodo, removeTodo }) => {
   return todos.map((todo, index) => (
     <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
-      <span className='number'>{todos.indexOf(todo) + 1}</span>
-      <input type="checkbox" onChange={() => completeTodo(todo.id)} key={todo.id}></input>
-      <div>{todo.text}</div>
+      <div>
+        <span className='number'>{todos.indexOf(todo) + 1}</span>
+        <input type="checkbox" onChange={() => completeTodo(todo.id)} key={todo.id}></input>
+      </div>
+      <div className="todo-text">{todo.text}</div>
       <DeleteOutlinedIcon 
         onClick={() => removeTodo(todo.id)}
         className='delete-icon'
